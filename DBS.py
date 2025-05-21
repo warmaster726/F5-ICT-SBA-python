1b9c01583808283c234e3b729f67850dc37b98cd
warmaster726 <83198872+warmaster726@users.noreply.github.com>
2025-05-21 20:25:58 +0800

    Update DBS.py

--- DBS.py
+++ DBS.py
@@ -1,23 +1,20 @@
 import sqlite3
 from datetime import datetime, timedelta
 
-current_date = datetime.now()
-
-connection = sqlite3.connect(r'Database\EMEAS.db')
-
-cursor = connection.cursor()
-
 def checking():
+    current_date = datetime.now()
+    connection = sqlite3.connect(r'Database\EMEAS.db')
+    cursor = connection.cursor()
     cursor.execute("SELECT MAX(dt) FROM metadata")
     largest_dt = cursor.fetchone()[0]
+
+    cursor.close()
+    connection.close()
 
     if current_date - timedelta(days=330) >= datetime.strptime(largest_dt, "%Y-%m-%d"):
         return True
     else:
         return False
-        
-cursor.close()
-connection.close()
-
+    
 
 
