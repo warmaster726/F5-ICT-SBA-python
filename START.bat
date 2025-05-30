98f0f3de8b6b4989906b677f9cd51be1d2994d80
warmaster726 <83198872+warmaster726@users.noreply.github.com>
2025-05-30 21:16:34 +0800

    DB update

--- START.bat
+++ START.bat
@@ -1,7 +1,7 @@
 @echo off
 pip install pip --upgrade
-pip install pandas
-pip install openpyxl
+pip install pandas --upgrade
+pip install openpyxl --upgrade
 cls
 rem Starting program, please wait...
 python Main.py
